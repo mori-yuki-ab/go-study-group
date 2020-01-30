dccd7c853eeca4ff5822bcb7a88ba43a428904b4
森 雄紀 <mori_yuki@applibot.co.jp>
2020-01-30 19:00:17 +0900

    chapter1課題

--- chapter1/kadai.go
+++ chapter1/kadai.go
@@ -1,5 +1,14 @@
 package chapter1
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/apbgo/go-study-group/chapter1/lib"
+)
+
 // Calc opには+,-,×,÷の4つが渡ってくることを想定してxとyについて計算して返却(正常時はerrorはnilでよい)
 // 想定していないopが渡って来た時には0とerrorを返却
 func Calc(op string, x, y int) (int, error) {
@@ -9,8 +18,25 @@ func Calc(op string, x, y int) (int, error) {
 	// https://golang.org/pkg/fmt/#Errorf
 
 	// TODO Q1
-
-	return 0, nil
+	result := 0
+	var err error
+	switch op {
+	case "+":
+		result = x + y
+	case "-":
+		result = x - y
+	case "×":
+		result = x * y
+	case "÷":
+		if y == 0 {
+			err = fmt.Errorf("integer divide by zero")
+		} else {
+			result = x / y
+		}
+	default:
+		err = fmt.Errorf("invalid op=%s", op)
+	}
+	return result, err
 }
 
 // StringEncode 引数strの長さが5以下の時キャメルケースにして返却、それ以外であればスネークケースにして返却
@@ -19,16 +45,34 @@ func StringEncode(str string) string {
 	// chapter1/libのToCamelとToSnakeを使うこと
 
 	// TODO Q2
-
-	return ""
+	if len(str) <= 5 {
+		return lib.ToCamel(str)
+	}
+	return lib.ToSnake(str)
 }
 
 // Sqrt 数値xが与えられたときにz²が最もxに近い数値zを返却
 func Sqrt(x float64) float64 {
 
 	// TODO Q3
+	z := 1.0
+	for !isApproximation(z*z, x) {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
 
-	return 0
+// 差が0.00000000000005以下ならtrueを返却する
+func isApproximation(x, y float64) bool {
+	difference := x - y
+	if difference < 0 {
+		difference *= -1
+	}
+	if difference < 0.00000000000005 {
+		// 差が既定値以下
+		return true
+	}
+	return false
 }
 
 // Pyramid x段のピラミッドを文字列にして返却
@@ -39,8 +83,22 @@ func Pyramid(x int) string {
 	// int -> stringはstrconv.Ioa() https://golang.org/pkg/strconv/#Itoa
 
 	// TODO Q4
+	if x <= 0 {
+		// 0以下はerrorを返却
+		return "error"
+	}
+	var result string = ""
+	for i := 1; i <= x; i++ {
+		if i != 1 {
+			// 2以上のときに改行つける
+			result += "\n"
+		}
+		for j := 1; j <= i; j++ {
+			result += strconv.Itoa(j)
+		}
+	}
 
-	return ""
+	return result
 }
 
 // StringSum x,yをintにキャストし合計値を返却 (正常終了時、errorはnilでよい)
@@ -51,8 +109,15 @@ func StringSum(x, y string) (int, error) {
 	// string -> intはstrconv.Atoi() https://golang.org/pkg/strconv/#Atoi
 
 	// TODO Q5
-
-	return 0, nil
+	iX, err := strconv.Atoi(x)
+	if err != nil {
+		return 0, err
+	}
+	iY, err := strconv.Atoi(y)
+	if err != nil {
+		return 0, err
+	}
+	return iX + iY, nil
 }
 
 // SumFromFileNumber ファイルを開いてそこに記載のある数字の和を返却
@@ -61,6 +126,25 @@ func SumFromFileNumber(filePath string) (int, error) {
 	// bufio.Scannerなどで１行ずつ読み込むと良い
 
 	// TODO Q6 オプション
-
-	return 0, nil
+	file, err := os.Open(filePath)
+	if err != nil {
+		// 開けない
+		return 0, err
+	}
+	defer file.Close()
+	result := 0
+	sc := bufio.NewScanner(file)
+	for i := 1; sc.Scan(); i++ {
+		if err := sc.Err(); err != nil {
+			// エラー処理
+			return 0, err
+		}
+		rowVal, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			// 数字じゃなかったら飛ばす
+			continue
+		}
+		result += rowVal
+	}
+	return result, nil
 }
